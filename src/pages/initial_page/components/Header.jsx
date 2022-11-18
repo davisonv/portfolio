59c0b3e8b692a64b4ef2737abb3710b97db846ff
davisonv <davison.vinicius@activesoftconsultoria.com.br>
2022-11-18 20:11:34 -0300

    implementacao do menu

--- src/pages/initial_page/components/Header.jsx
+++ src/pages/initial_page/components/Header.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import styled from 'styled-components'
+import { slide as Menu } from 'react-burger-menu'
 
 import HeaderImage from '../../../assets/images/HeaderImage1.png'
 
@@ -14,9 +15,73 @@ const HeaderContainer = styled.div`
   width: 100vw;
 `
 
+const ItemMenu = styled.a`
+  display: flex;
+  align-items: center;
+`
+
 function Header () {
+
+  const styles = {
+    bmBurgerButton: {
+      position: 'fixed',
+      width: '30px',
+      height: '25px',
+      left: '28px',
+      top: '28px'
+    },
+    bmBurgerBars: {
+      background: '#f0f5f8'
+    },
+    bmBurgerBarsHover: {
+      background: '#1965ff'
+    },
+    bmCrossButton: {
+      height: '24px',
+      width: '24px'
+    },
+    bmCross: {
+      background: '#bdc3c7'
+    },
+    bmMenuWrap: {
+      position: 'fixed',
+      height: '100%'
+    },
+    bmMenu: {
+      background: '#0a263d',
+      padding: '2.5em 1.5em 0',
+      fontSize: '1.15em'
+    },
+    bmMorphShape: {
+      fill: '#373a47'
+    },
+    bmItemList: {
+      color: '#b8b7ad',
+      padding: '0.8em',
+      display: 'flex',
+      flexDirection: 'column'
+    },
+    bmItem: {
+      display: 'inline-block',
+      textDecoration: 'none',
+      color: '#f0f5f8'
+    },
+    bmOverlay: {
+      background: 'rgba(14, 0, 207, 0.3)'
+    }
+  }
+
   return (
-    <HeaderContainer>
+    <>
+      <Menu styles={styles}>
+        <a id="home" href="/">
+            Home
+        </a>
+        <a id="about" href="/about">About</a>
+        <a id="contact" href="/contact">Contact</a>
+      </Menu>
+
+      <HeaderContainer>
       <h1 
         style={{
           color: '#fff',
@@ -28,6 +93,8 @@ function Header () {
         Elevando negócios no universo digital.
       </h1>
     </HeaderContainer>
+    </>
+    
   )
 }
 
